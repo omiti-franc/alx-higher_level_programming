205944f446640c2a6aae0b18e50eac769d6878b9
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2022-11-30 16:45:59 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
-(0, 10)
+for i in range(100):
+    if i < 99:
+        print('{:02d}, '.format(i), end='')
+    else:
+        print(i)
