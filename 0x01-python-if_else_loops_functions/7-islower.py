d5707f5b1053c394d32fff23ceca38f3fa3160eb
omiti-franc <omitifranc@gmail.com>
2022-11-30 06:33:14 -0800

    Task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# islower - function that checks for lowercase character
+# Return - True if c is lowercase otherwise, False
+def islower(c):
+    if 123 > ord(c) > 96:
+        return True
+    else:
+        False
