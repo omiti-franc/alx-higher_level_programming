5be5aaf20e872fef23889dc1fd34fe4062d980dc
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2023-01-10 16:06:15 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,38 +1,19 @@
 #!/usr/bin/python3
-import json
+"""Add item script."""
+import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Writes object in JSON representation to a file
-    Args:
-        my_obj (object): object to write
-        filename (str): path to a file
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+try:
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
 
+argc = len(sys.argv)
 
-if __name__ == '__main__':
-    filename = "my_list.json"
-    my_list = [1, 2, 3]
-    save_to_json_file(my_list, filename)
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
 
-    filename = "my_dict.json"
-    my_dict = {
-        'id': 12,
-        'name': "John",
-        'places': ["San Francisco", "Tokyo"],
-        'is_active': True,
-        'info': {
-            'age': 36,
-            'average': 3.14
-        }
-    }
-    save_to_json_file(my_dict, filename)
-
-    try:
-        filename = "my_set.json"
-        my_set = {132, 3}
-        save_to_json_file(my_set, filename)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+save_to_json_file(lst, "add_item.json")
