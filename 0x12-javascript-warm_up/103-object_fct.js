2f4950850906e79a686a65550991d4bf2eb84682
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2023-03-13 21:46:32 +0300

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -9,3 +9,10 @@ console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
